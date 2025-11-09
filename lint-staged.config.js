1c4d8cc11afd1d9ab82d5fd85eb4a198ce7396aa
Roman <romanrostislavovich@gmail.com>
2025-11-09 16:00:58 +0300

    fix: add stylelint command

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,10 @@
 export default {
   '**/*.{js,jsx,ts,tsx,svelte}': ['eslint --fix', 'prettier --write'],
-  '**/*.{css,scss,vue}': ['prettier --write'],
+  '**/*.{css,scss,sass}': [
+    'stylelint **/*.{scss,sass,css} -c ./stylelint.config.js --fix',
+    'prettier --write',
+  ],
   '**/*.{json}': ['prettier --write'],
   '**/*.{md}': ['markdownlint-cli2 -c ./.markdownlint.json'],
-  '**/*.{html}': [],
+  '**/*.{html}': ['htmlhint **/*.html'],
 };
--- src/packageJSON.ts
+++ src/packageJSON.ts
@@ -7,11 +7,13 @@ const commandRunDictionary = new Map<Tools, string>([
   [Tools.Prettier, 'prettier --write'],
   [Tools.HTMLHint, 'htmlhint **/*.html'],
   [Tools.MarkdownLint, 'markdownlint-cli2 -c ./.markdownlint.json'],
+  [Tools.Stylelint, 'stylelint **/*.{scss,sass,css} -c ./stylelint.config.js --fix'],
 ]);
 
 const commandNameDictionary = new Map<Tools, string>([
   [Tools.HTMLHint, 'lint:html'],
   [Tools.Prettier, 'format:prettier'],
+  [Tools.Stylelint, 'lint:style'],
   [Tools.MarkdownLint, 'lint:markdown'],
 ]);
 
@@ -28,6 +30,11 @@ export function updatePackageJSON(answers: Prompt & { manager: PackageManagers }
   const packageJSON = getPackageJSON();
 
   packageJSON.scripts['lint:eslint'] = 'eslint --fix';
+
+  if (answers?.tools.includes(Tools.Stylelint)) {
+    packageJSON.scripts[commandNameDictionary.get(Tools.Stylelint) || ''] =
+      commandRunDictionary.get(Tools.Stylelint);
+  }
 
   if (answers?.tools.includes(Tools.Prettier)) {
     packageJSON.scripts[commandNameDictionary.get(Tools.Prettier) || ''] = commandRunDictionary.get(
